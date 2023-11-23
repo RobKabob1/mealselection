ebf6ff030a9cb4e3c25c6472e7abb34fffde80f9
“rwp005” <rwp005@gmail.com>
2023-11-23 09:38:35 -0600

    Added Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Meal Selection'),
     );
   }
 }
@@ -30,39 +31,62 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        centerTitle: true,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            //breakfast
+            Row(
+              //mainAxisAlignment: Center,
+              children: [
+                Expanded(
+                  child: IconButton(
+                    iconSize: 20,
+                    icon: Image.asset('images/breakfast.png',
+                        fit: BoxFit.fitWidth),
+                    onPressed: () {},
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            //lunch
+            Row(
+              //mainAxisAlignment: Center,
+              children: [
+                Expanded(
+                  child: IconButton(
+                    iconSize: 20,
+                    icon: Image.asset('images/breakfast.png',
+                        fit: BoxFit.scaleDown),
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+            //dinner
+            Row(
+              //mainAxisAlignment: Center,
+              children: [
+                Expanded(
+                  child: IconButton(
+                    iconSize: 20,
+                    icon: Image.asset('images/breakfast.png',
+                        fit: BoxFit.contain),
+                    onPressed: () {},
+                  ),
+                ),
+              ],
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
