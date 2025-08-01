9ee40449d0ac959c1bf14203bf9808aad2b958f9
“rwp005” <rwp005@gmail.com>
2025-08-01 01:33:34 -0500

    Modified blue color

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 const mobileBackgroundColor = Color.fromRGBO(0, 0, 0, 1);
 const webBackgroundColor = Color.fromRGBO(18, 18, 18, 1);
 const mobileSearchColor = Color.fromRGBO(38, 38, 38, 1);
-const blueColor = Color.fromRGBO(0, 149, 246, 1);
+const blueColor = Color.fromRGBO(33, 88, 124, 1);
 const redColor = Color.fromARGB(197, 248, 1, 1);
 const primaryColor = Colors.white;
 const secondaryColor = Colors.grey;
